3a4c647251af8f38d1d81a5e6b91ec7109042ce0
Gordon Chen <gordonbchen@gmail.com>
2023-05-18 15:50:41 -0400

    add absolute sensor config

--- src/main/java/frc/robot/subsystems/swerve/steer/SteerMotor.java
+++ src/main/java/frc/robot/subsystems/swerve/steer/SteerMotor.java
@@ -3,7 +3,9 @@ package frc.robot.subsystems.swerve.steer;
 public interface SteerMotor {
     public void setTargetPositionDegrees(double targetPositionDegrees);
 
-    public double getPositionTicks();
+    public double getRelativePositionTicks();
+    public double getAbsolutePositionTicks();
+    
     public double getPositionDegrees();
 
     public double getTicksPerRev();
--- src/main/java/frc/robot/subsystems/swerve/steer/TalonSRXSteerMotor.java
+++ src/main/java/frc/robot/subsystems/swerve/steer/TalonSRXSteerMotor.java
@@ -26,7 +26,8 @@ public class TalonSRXSteerMotor implements SteerMotor, Loggable {
     private static final boolean INVERT_MOTOR = false;              // TODO: verify inversion.
 
     // PID.
-    private static final int K_PID_LOOP = 0;
+    private static final int K_RELATIVE_PID_LOOP = 0;
+    private static final int K_ABSOLUTE_PID_LOOP = 1;
     private static final int K_PID_SLOT = 0;
     private static final int K_TIMEOUT_MS = 10;
 
@@ -61,11 +62,16 @@ public class TalonSRXSteerMotor implements SteerMotor, Loggable {
         m_motor.configSupplyCurrentLimit(talonCurrentLimit);
 
         configRelativeSensor();
+        configAbsoluteSensor();
+
+        // Set the relative encoder to start at the inital wheel position.
+        double wheelPositionTicks = getAbsolutePositionTicks() + Constants.SwerveModules.WHEEL_ZERO_OFFSET_TICKS[location];
+        m_motor.setSelectedSensorPosition(wheelPositionTicks, K_RELATIVE_PID_LOOP, K_TIMEOUT_MS);
     }
 
     private void configRelativeSensor() {
         // Select which sensor to configure.
-        m_motor.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.CTRE_MagEncoder_Relative, K_PID_LOOP, K_TIMEOUT_MS);
+        m_motor.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.CTRE_MagEncoder_Relative, K_RELATIVE_PID_LOOP, K_TIMEOUT_MS);
 
         // Set deadband to minimum.
         m_motor.configNeutralDeadband(PERCENT_DEADBAND, K_TIMEOUT_MS);
@@ -73,9 +79,11 @@ public class TalonSRXSteerMotor implements SteerMotor, Loggable {
         configInversion(INVERT_SENSOR_PHASE, INVERT_MOTOR);
         configPIDs(PID_GAINS);
         configMotionMagic(MAX_ACCELERATION, MAX_VELOCITY);
+    }
 
-        // Set the relative encoder to start at the inital wheel position.
-        m_motor.setSelectedSensorPosition(getInitialWheelPositionTicks(), K_PID_LOOP, K_TIMEOUT_MS);
+    private void configAbsoluteSensor() {
+        m_motor.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.CTRE_MagEncoder_Absolute, K_ABSOLUTE_PID_LOOP, K_TIMEOUT_MS);
+       configInversion(INVERT_SENSOR_PHASE, INVERT_MOTOR);
     }
     
     private void configInversion(boolean invertSensorPhase, boolean invertMotor) {
@@ -91,7 +99,7 @@ public class TalonSRXSteerMotor implements SteerMotor, Loggable {
         m_motor.configPeakOutputReverse(-1.0 * gains.kPeakOutput, K_TIMEOUT_MS);
 
         // Set gains.
-        m_motor.selectProfileSlot(K_PID_SLOT, K_PID_LOOP);
+        m_motor.selectProfileSlot(K_PID_SLOT, K_RELATIVE_PID_LOOP);
         m_motor.config_kF(K_PID_SLOT, gains.kF, K_TIMEOUT_MS);
         m_motor.config_kP(K_PID_SLOT, gains.kP, K_TIMEOUT_MS);
         m_motor.config_kI(K_PID_SLOT, gains.kI, K_TIMEOUT_MS);
@@ -105,41 +113,34 @@ public class TalonSRXSteerMotor implements SteerMotor, Loggable {
         configPIDs(gains);
     }
 
-    private void configMotionMagic(double maxAccelerationTicksPer100msPerSecond, double maxVelocityTicksPer100ms) {
-        m_motor.configMotionAcceleration(maxAccelerationTicksPer100msPerSecond);
-        m_motor.configMotionCruiseVelocity(maxVelocityTicksPer100ms);
-    }
-
-    private double getInitialWheelPositionTicks() {
-        double initalAbsoluteEncoderPositionTicks = m_motor.getSensorCollection().getPulseWidthPosition();
-        return initalAbsoluteEncoderPositionTicks + Constants.SwerveModules.WHEEL_ZERO_OFFSET_TICKS[location];
+    private void configMotionMagic(double maxAcceleration, double maxVelocity) {
+        m_motor.configMotionAcceleration(maxAcceleration);
+        m_motor.configMotionCruiseVelocity(maxVelocity);
     }
 
     public double getTicksPerRev() {
         return TICKS_PER_REV;
     }
 
-    public double getPositionTicks() {
-        return m_motor.getSelectedSensorPosition(K_PID_LOOP);
+    public double getRelativePositionTicks() {
+        return m_motor.getSelectedSensorPosition(K_RELATIVE_PID_LOOP);
+    }
+
+    public double getAbsolutePositionTicks() {
+        return m_motor.getSelectedSensorPosition(K_ABSOLUTE_PID_LOOP);
     }
 
     public double getPositionDegrees() {
-        double rawDegrees = Conversions.ticksToDegrees(getPositionTicks(), TICKS_PER_REV, GEAR_RATIO);
-        double degrees = Conversions.shiftHalfCircle(rawDegrees);
+        double degrees = Conversions.ticksToDegrees(getRelativePositionTicks(), TICKS_PER_REV, GEAR_RATIO);
         return degrees;
     }
     
     public void setTargetPositionDegrees(double targetPositionDegrees) {
-        double currPositionDegrees = getPositionDegrees();
+        double targetPositionTicks = Conversions.degreesToTicks(targetPositionDegrees, TICKS_PER_REV, GEAR_RATIO);
+        setTargetPositionTicks(targetPositionTicks);
+    }
 
-        // Optimize target angle.
-        targetPositionDegrees = Conversions.optimizeTargetAngle(targetPositionDegrees, currPositionDegrees);
-
-        // Find tick change needed.
-        double deltaDegrees = targetPositionDegrees - currPositionDegrees;
-        double deltaTicks = Conversions.degreesToTicks(deltaDegrees, TICKS_PER_REV, GEAR_RATIO);
-
-        double targetTicks = getPositionTicks() + deltaTicks;
-        m_motor.set(TalonSRXControlMode.MotionMagic, targetTicks);
+    public void setTargetPositionTicks(double targetPositionTicks) {
+        m_motor.set(TalonSRXControlMode.MotionMagic, targetPositionTicks);
     }
 }
