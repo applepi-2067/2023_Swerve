4059c090bdc188c1dffff89d6fc177532922727d
Gordon Chen <gordonbchen@gmail.com>
2023-09-28 18:47:25 -0400

    add magic motion todo

--- src/main/java/frc/robot/subsystems/swerve/drive/TalonFXDriveMotor.java
+++ src/main/java/frc/robot/subsystems/swerve/drive/TalonFXDriveMotor.java
@@ -14,6 +14,7 @@ import io.github.oblarg.oblog.annotations.Log;
 
 public class TalonFXDriveMotor implements DriveMotor, Loggable {
     // TODO: Find physical limits.
+    // TODO: Magic motion.
 
     private WPI_TalonFX m_motor;
 
