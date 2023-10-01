30658e69f98ebb161aac40142fe6131045332580
Gordon Chen <gordonbchen@gmail.com>
2023-10-01 17:20:59 -0400

    add motor rotations to degrees conversions

--- src/main/java/frc/robot/utils/Conversions.java
+++ src/main/java/frc/robot/utils/Conversions.java
@@ -1,7 +1,6 @@
 package frc.robot.utils;
 
 public class Conversions {
-    private static final double DEGREES_PER_REV = 360.0;
 
     public static double RPMToTicksPer100ms(double RPM, double ticksPerRev) {
         return RPM * ticksPerRev / (60.0 * 10.0);
@@ -9,14 +8,6 @@ public class Conversions {
 
     public static double ticksPer100msToRPM(double ticksPer100ms, double ticksPerRev) {
         return ticksPer100ms * (10.0 * 60.0) * (1.0 / ticksPerRev);
-    }    
-
-    public static double ticksToDegrees(double ticks, double ticksPerRev, double gearRatio) {
-       return ticks * (1.0 / ticksPerRev) * (1.0 / gearRatio) * DEGREES_PER_REV;
-    }
-
-    public static double degreesToTicks(double degrees, double ticksPerRev, double gearRatio) {
-        return degrees * (1.0 / DEGREES_PER_REV) * gearRatio * ticksPerRev;
     }
 
     public static double metersPerSecondToRPM(double metersPerSecond, double radiusMeters) {
@@ -25,5 +16,13 @@ public class Conversions {
 
     public static double rpmToMetersPerSecond(double RPM, double radiusMeters) {
         return RPM * (1.0 / 60.0) * (2 * Math.PI) * radiusMeters;
+    }
+
+    public static double motorRotationsToDegrees(double motorRotations, double gearRatio) {
+        return (motorRotations / gearRatio) * 360.0;
+    }
+
+    public static double degreesToMotorRotations(double degrees, double gearRatio) {
+        return (degrees / 360.0) * gearRatio;
     }
 }
