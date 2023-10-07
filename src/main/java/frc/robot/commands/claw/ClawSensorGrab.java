472b1b80b0737dfe9233bba1e20ff29654eb682b
Gordon Chen <gordonbchen@gmail.com>
2023-10-07 16:23:43 -0400

    clean up ClawSensorGrab command

--- src/main/java/frc/robot/commands/claw/ClawSensorGrab.java
+++ src/main/java/frc/robot/commands/claw/ClawSensorGrab.java
@@ -1,24 +1,18 @@
 package frc.robot.commands.claw;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.Claw;
 
 
 public class ClawSensorGrab extends SequentialCommandGroup {
-  private final Claw m_claw;
-
   public ClawSensorGrab() {
-    m_claw = Claw.getInstance();
-
-    addRequirements(m_claw);
     addCommands(
       new OpenCloseClaw(true),
       new SetClawBeltSpeed(0.8),
 
       new WaitUntilCommand(
-        () -> m_claw.getIRSensorTriggered()
+        () -> Claw.getInstance().getIRSensorTriggered()
       ),
 
       new OpenCloseClaw(false),
