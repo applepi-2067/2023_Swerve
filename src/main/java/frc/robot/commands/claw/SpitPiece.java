72a911e88895003af02f94d159a979dba03e57fa
Gordon Chen <gordonbchen@gmail.com>
2023-10-07 17:01:27 -0400

    automate piece spitting - reduce spit speed - stow after spitting

--- src/main/java/frc/robot/commands/claw/SpitPiece.java
+++ src/main/java/frc/robot/commands/claw/SpitPiece.java
@@ -1,13 +1,26 @@
 package frc.robot.commands.claw;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.commands.SetArmShoulderPosition;
+import frc.robot.constants.ScoringPositions;
+import frc.robot.subsystems.Claw;
 
 
 public class SpitPiece extends SequentialCommandGroup {
   public SpitPiece() {
     addCommands(
       new OpenCloseClaw(true),
-      new SetClawBeltSpeed(-1.0)
+      new SetClawBeltSpeed(-0.5),
+
+      new WaitUntilCommand(
+        () -> !Claw.getInstance().getIRSensorTriggered()
+      ),
+
+      new SetClawBeltSpeed(0.0),
+      new OpenCloseClaw(false),
+
+      new SetArmShoulderPosition(ScoringPositions.STOW)
     );
   }
 }
