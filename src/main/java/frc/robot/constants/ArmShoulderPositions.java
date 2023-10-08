dc39369c5c450f07398f8e839239bd3e8baf1fa0
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-10-08 11:04:52 -0400

    update ArmShoulderPositions

--- src/main/java/frc/robot/constants/ArmShoulderPositions.java
+++ src/main/java/frc/robot/constants/ArmShoulderPositions.java
@@ -9,7 +9,7 @@ public final class ArmShoulderPositions {
 
     public static class Cube {
         public static final ArmShoulderPosition MID = new ArmShoulderPosition(0.2748, -63.46);
-        public static final ArmShoulderPosition HIGH = new ArmShoulderPosition(0.7346, -60.27);
+        public static final ArmShoulderPosition HIGH = new ArmShoulderPosition(0.75, -57.5);
     }
 
     public static class Cone {
@@ -18,7 +18,7 @@ public final class ArmShoulderPositions {
     }
 
     public static class Pickup {
-        public static final ArmShoulderPosition GROUND = new ArmShoulderPosition(0.0, 0.0);
-        public static final ArmShoulderPosition DOUBLE_SUBSTATION = new ArmShoulderPosition(0.0, 0.0);
+        public static final ArmShoulderPosition GROUND = new ArmShoulderPosition(0.0, -110.3);
+        public static final ArmShoulderPosition DOUBLE_SUBSTATION = new ArmShoulderPosition(0.858, -63.0);
     }
 }
