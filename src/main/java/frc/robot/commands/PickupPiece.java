91d6f4ca1841c2da73127cfae204fead33514669
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-10-08 10:24:12 -0400

    stow after piece pickup

--- src/main/java/frc/robot/commands/PickupPiece.java
+++ src/main/java/frc/robot/commands/PickupPiece.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.claw.ClawSensorGrab;
+import frc.robot.constants.ArmShoulderPositions;
 import frc.robot.utils.ArmShoulderPosition;
 
 
@@ -9,7 +10,8 @@ public class PickupPiece extends SequentialCommandGroup {
   public PickupPiece(ArmShoulderPosition armShoulderPosition) {
     addCommands(
       new SetArmShoulderPosition(armShoulderPosition),
-      new ClawSensorGrab()
+      new ClawSensorGrab(),
+      new SetArmShoulderPosition(ArmShoulderPositions.STOW)
     );
   }
 }
