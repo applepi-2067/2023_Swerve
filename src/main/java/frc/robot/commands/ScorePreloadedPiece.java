733042128fd1a4c74e9730fd654099523e60413d
Gordon Chen <gordonbchen@gmail.com>
2023-10-10 17:09:52 -0400

    add ScorePreloadedPiece auto

---
+++ src/main/java/frc/robot/commands/ScorePreloadedPiece.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.claw.OpenCloseClaw;
+import frc.robot.commands.claw.SetClawBeltSpeed;
+import frc.robot.commands.claw.SpitPiece;
+import frc.robot.constants.ArmShoulderPositions;
+
+public class ScorePreloadedPiece extends SequentialCommandGroup{
+    public ScorePreloadedPiece () {
+        addCommands(
+            // Grip cone.
+            new OpenCloseClaw(false),
+            new SetClawBeltSpeed(0.2),
+
+            // Raise arm, drop cone, and stow.
+            new SetArmShoulderPosition(ArmShoulderPositions.Cone.HIGH),
+            new SpitPiece()
+        );
+    }
+}
