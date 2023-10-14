fddd42386da382f987fb58c2eedca9edb7de5fd1
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-10-14 08:29:19 -0400

    fix gyro sign

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -17,8 +17,7 @@ public class AutoBalance extends CommandBase {
 
   @Override
   public void execute() {
-    // TODO: Check sign.
-    double leftStickY = Math.sin(-1.0 * m_drivetrain.getGyroPitch()) / 2.0;
+    double leftStickY = Math.sin(m_drivetrain.getGyroPitch()) / 2.0;
     m_drivetrain.drive(0.0, leftStickY, 0.0);
   }
 
