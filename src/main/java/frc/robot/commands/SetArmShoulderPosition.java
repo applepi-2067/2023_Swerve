3a026cf1f9c998a39798659973d3b2ff8e0a7fde
Alex Arpie <119821293+Devallex@users.noreply.github.com>
2023-10-24 20:06:22 -0400

    changes

--- src/main/java/frc/robot/commands/SetArmShoulderPosition.java
+++ src/main/java/frc/robot/commands/SetArmShoulderPosition.java
@@ -12,19 +12,15 @@ public class SetArmShoulderPosition extends SequentialCommandGroup {
   }
 
   public SetArmShoulderPosition(ArmShoulderPosition armShoulderPosition, boolean stowFirst) {
+    SetArmPosition thirdCommand = null;
     if (stowFirst) {
-      addCommands(
-        new SetArmPosition(0.0),
-        new SetShoulderPosition(armShoulderPosition.m_shoulderAngleDegrees),
-        new SetArmPosition(armShoulderPosition.m_armPositionMeters)
-      );
+      thirdCommand = new SetArmPosition(0.0);
     }
 
-    else {
-      addCommands(
-        new SetShoulderPosition(armShoulderPosition.m_shoulderAngleDegrees),
-        new SetArmPosition(armShoulderPosition.m_armPositionMeters)
-      );
-    }
+    addCommands(
+      new SetShoulderPosition(armShoulderPosition.m_shoulderAngleDegrees),
+      new SetArmPosition(armShoulderPosition.m_armPositionMeters),
+      thirdCommand
+    );
   }
 }
