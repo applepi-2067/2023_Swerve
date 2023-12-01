7b5a434c29636e7a2452533b1634fb2bacdded92
Tyler Reid <reidcore11@gmail.com>
2023-12-01 00:49:58 -0500

    Relocated the AutoBuilder

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -99,6 +99,19 @@ public class Drivetrain extends SubsystemBase implements Loggable {
     SmartDashboard.putData("Field", m_field);
 
     m_pose = getRobotPose2d();
+
+    AutoBuilder.configureHolonomic(
+      this::getRobotPose2d, // Robot pose supplier
+      this::resetGyro, // Method to reset odometry (will be called if your auto has a starting pose)
+      this::ChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+      this::fromFieldRelativeSpeeds, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+      new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+        new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+        4.5, // Max module speed, in m/s
+        0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+        new ReplanningConfig() // Default path replanning config. See the API for the options here
+    )
   }
 
   // Log state.
@@ -216,17 +229,4 @@ public class Drivetrain extends SubsystemBase implements Loggable {
     m_pose = getRobotPose2d();
     m_field.setRobotPose(m_pose);
   }
-    AutoBuilder.configureHolonomic(
-      getRobotPose2d, // Robot pose supplier
-      resetGyro, // Method to reset odometry (will be called if your auto has a starting pose)
-      ChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-      fromFieldRelativeSpeeds, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-      new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
-        new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
-        4.5, // Max module speed, in m/s
-        0.4, // Drive base radius in meters. Distance from robot center to furthest module.
-        new ReplanningConfig() // Default path replanning config. See the API for the options here
-    )
-  )
 }
